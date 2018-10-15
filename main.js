ed4a89cde1135945c4e1353ea8c58264a368e8d1
gaidmakua <jmazurevich@gmail.com>
2018-10-15 10:36:42 +0300

    Added body types

--- main.js
+++ main.js
@@ -1,32 +1,93 @@
-var ctx;
+var SexEnum = Object.freeze({
+  "female": 0,
+  "male": 1
+});
+var RaceEnum = Object.freeze({
+  "human": 0,
+  "orc": 1,
+  "skeleton": 2
+})
+
+let RACE_STATE = RaceEnum.human;
+
+var SEX_STATE = SexEnum.male;
 
 function setup() {
   var canvas = document.getElementById('spritesheet');
-  ctx = canvas.getContext('2d');
-
-  var femaleButton = document.getElementById('female')
-  femaleButton.onclick = function() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawImage('body_female.png');
-    femaleButton.className = "btn btn-primary"
-    maleButton.className = "btn btn-outline-primary"
+  painter = {
+    width: canvas.width,
+    height: canvas.height,
+    ctx: canvas.getContext('2d'),
+    drawImage: function(url) {
+      var ctx = this.ctx;
+      var image = new Image();
+      image.onload = function() {
+        ctx.drawImage(image, 0, 0);
+      };
+      image.src = url;
+    },
+    clear: function() {
+      this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
   }
-  var maleButton = document.getElementById('male')
-  maleButton.onclick = function() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawImage('body_male.png');
-    maleButton.className = "btn btn-primary"
-    femaleButton.className = "btn btn-outline-primary"
-  }
 
-  drawImage('body_male.png');
+  initSexSwitch(painter)
+
+  painter.drawImage('male_human.png');
 }
 window.onload = setup;
 
-function drawImage(url) {
-  var image = new Image();
-  image.onload = function() {
-    ctx.drawImage(image, 0, 0);
-  };
-  image.src = url;
+function initSexSwitch(painter) {
+  var femaleConfig = {
+    button: document.getElementById('female'),
+    image: 'human_female.png',
+    state: SexEnum.female
+  }
+  var maleConfig = {
+    button: document.getElementById('male'),
+    image: 'human_male.png',
+    state: SexEnum.male
+  }
+}
+
+function switchSex(sex) {
+  painter.clear();
+  $('#female')[0].className = "btn btn-outline-primary";
+  $('#male')[0].className = "btn btn-outline-primary";
+  $(`#${sex}`).addClass('btn-primary').removeClass('btn-outline-primary');
+  switch (sex) {
+    case 'male':
+      painter.drawImage('male_human.png');
+      SEX_STATE = SexEnum.male;
+      break;
+    case 'female':
+      painter.drawImage('female_human.png');
+      SEX_STATE = SexEnum.female;
+      break;
+  }
+}
+
+function switchRace(race) {
+  console.log('switch to race', race);
+  painter.clear();
+  $('#human')[0].className = "btn btn-outline-primary";
+  $('#orc')[0].className = "btn btn-outline-primary";
+  $('#skeleton')[0].className = "btn btn-outline-primary";
+  $(`#${race}`).addClass('btn-primary').removeClass('btn-outline-primary');
+  let currentSexValue = Object.keys(SexEnum).filter(sex => SexEnum[sex] === SEX_STATE)[0];
+  let pictureName = `${currentSexValue}_${race}.png`;
+  switch (race) {
+    case 'human':
+        RACE_STATE = RaceEnum.human;
+        break;
+    case 'orc':
+        RACE_STATE = RaceEnum.orc;
+        break;
+    case 'skeleton':
+        RACE_STATE = RaceEnum.skeleton;
+        pictureName = `male_${race}.png`;
+        break;
+  }
+  painter.drawImage(pictureName);
+
 }
